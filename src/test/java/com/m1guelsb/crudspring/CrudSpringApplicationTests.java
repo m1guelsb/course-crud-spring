0fbfd746400258ee15cdf973f63ed3a67f7ea053
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-05-19 01:17:15 -0300

    Initial commit

---
+++ src/test/java/com/m1guelsb/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1guelsb.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
