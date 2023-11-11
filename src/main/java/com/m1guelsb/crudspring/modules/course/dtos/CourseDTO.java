7a24bcf86ea71ef5b3056baf03818ee75f31dda7
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-11-11 15:42:01 -0300

    [feat] add enum validator

--- src/main/java/com/m1guelsb/crudspring/modules/course/dtos/CourseDTO.java
+++ src/main/java/com/m1guelsb/crudspring/modules/course/dtos/CourseDTO.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.m1guelsb.crudspring.modules.course.enums.CategoryEnum;
+import com.m1guelsb.crudspring.modules.course.enums.validation.ValueOfEnum;
 import com.m1guelsb.crudspring.modules.lesson.dtos.LessonDTO;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record CourseDTO(String id, @NotNull @NotBlank @Length(min = 3, max = 100) String name,
-		@NotNull @Length(max = 10) String category, List<LessonDTO> lessons) {
+public record CourseDTO(
+		String id,
+		@NotBlank @NotNull @Length(min = 5, max = 100) String name,
+		@NotNull @Length(max = 10) @ValueOfEnum(enumClass = CategoryEnum.class) String category,
+		@NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
